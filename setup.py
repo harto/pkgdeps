66ea224ff4c000ff5089de7a944554ae882df7f6
Stuart Campbell <stuart@harto.org>
2017-12-08 13:12:14 -0800

    Release helpers

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-from setuptools import setup
+from os import popen
+from setuptools import setup, find_packages
+import re
 
-setup(name='pkgdeps',
-      version='0.1',
-      description="Explore dependencies of a Python package by parsing its imports.",
-      url='https://github.com/harto/pkgdeps',
-      entry_points={
-          'console_scripts': ['pkgdeps=pkgdeps.__init__:main']
-      })
+package = 'pkgdeps'
+
+version = popen('git describe --tags').read().strip()
+if not re.match(r'^\d+\.\d+(\.\d+)?$', version):
+    raise Exception('unexpected version: %s' % version)
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name=package,
+    version=version,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['pkgdeps = pkgdeps:main']
+    },
+    description="Explore dependencies of a Python package by parsing its imports.",
+    long_description=long_description,
+    license='MIT',
+    keywords='python package dependency',
+    author='Stuart Campbell',
+    author_email='stuart@harto.org',
+    url='https://github.com/harto/pkgdeps',
+)
